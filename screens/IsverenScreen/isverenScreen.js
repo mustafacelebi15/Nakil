5f1eea55a7d30ad0171871de7dc56bc4288182d1
mustafacelebi15 <96596130+mustafacelebi15@users.noreply.github.com>
2024-03-20 07:05:23 +0300

    Adverts

--- screens/IsverenScreen/isverenScreen.js
+++ screens/IsverenScreen/isverenScreen.js
@@ -1,12 +1,88 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useEffect} from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import firestore from '@react-native-firebase/firestore';
 
-const TasiyiciScreen = () => {
+import Button from '../../Components/Button';
+import styles from "./isverenScreen.style";
+
+const isverenScreen = ({ navigation }) => {
+  const [advertiser, setAdvertiser] = useState('');
+  const [carrier, setCarrier] = useState('');
+  const [confirmation, setConfirmation] = useState(false);
+  const [from, setFrom] = useState('');
+  const [price, setPrice] = useState('');
+  const [where, setWhere] = useState('');
+  const [ontheway, setOntheway] = useState(false);
+  
+  useEffect(() => {
+    getEmailAndFetchUser();
+  }, []);
+ 
+  const getEmailAndFetchUser = async () => {
+    try {
+      const userEmail = await AsyncStorage.getItem('@email');
+      setAdvertiser(userEmail);
+      console.log(email);
+    } catch (error) {
+      console.error('Error getting email:', error);
+    }
+  };
+
+  
+  const removeLoginInfo = async () => {
+    try {
+      await AsyncStorage.removeItem('@email');
+      await AsyncStorage.removeItem('@password');
+      await AsyncStorage.removeItem('@hasLogin');
+      navigation.navigate('SignUp');
+    } catch (error) {
+      console.error('Error removing login information:', error);
+    }
+  };
+
+  const addAdvert = async () => {
+    try {
+      await firestore().collection('Nakils').add({
+        advertiser,
+        carrier,
+        confirmation,
+        from,
+        ontheway,
+        price,
+        where,
+      });
+      console.log('nakils added!');
+    } catch (error) {
+      console.error('Error adding user: ', error);
+    }
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Merhaba İsveren</Text>
+    <View style={styles.container}>
+      <View style={styles.contentContainer}>
+      <TextInput style={styles.input}
+        placeholder="From"
+        value={from}
+        onChangeText={text => setFrom(text)}
+      />
+      <TextInput style={styles.input}
+        placeholder="Where"
+        value={where}
+        onChangeText={text => setWhere(text)}
+      />
+      <TextInput style={styles.input}
+        placeholder="Price"
+        value={price}
+        onChangeText={text => setPrice(text)}
+      />
+      <View style={styles.ButtonContainer}>
+      <Button title={"İlan oluştur."} onPress={addAdvert}/>
+      <Button title={"Çıkış Yap"} onPress={removeLoginInfo}/>
+      </View>
+      </View>
     </View>
   );
-};
+}
 
-export default TasiyiciScreen;
+export default isverenScreen;
